54b9f110d15e9b4281ee49f23aa6c9fea369d739
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-23 18:32:05 +0300

    added common classes

---
+++ src/ECourses.Data/Common/Enums/RepositoryOperationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace ECourses.Data.Common.Enums
+{
+    public enum RepositoryOperationResult
+    {
+        Failure = 1,
+        Success = 2,
+    }
+}
---
+++ src/ECourses.Data/Common/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECourses.Data.Common.Enums;
+
+namespace ECourses.Data.Common.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<T?> GetById(Guid id);
+        Task<IEnumerable<T>> GetAll();
+        Task<RepositoryOperationResult> Create(T entity);
+        Task<RepositoryOperationResult> Update(Guid id, T entity);
+        Task<RepositoryOperationResult> Delete(Guid id);
+        Task<IEnumerable<T>> GetByField<TField>(Func<T, TField> fieldSelector);
+    }
+}
