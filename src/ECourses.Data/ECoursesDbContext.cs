721b06db0fb73afeff88558474fbd800b1ba6be7
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-23 18:32:23 +0300

    added DbContext

---
+++ src/ECourses.Data/ECoursesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ECourses.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECourses.Data
+{
+    public class ECoursesDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public ECoursesDbContext(DbContextOptions<ECoursesDbContext> options) : base(options) { }
+    }
+}
