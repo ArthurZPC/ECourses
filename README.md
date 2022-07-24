cb19d401fbab8aefb317752b7cdf524690d0d1b1
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-24 18:46:36 +0300

    initial database setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ECourses
+
 ## This DEMO app created for selling courses using ASP.NET Core
+
+### Steps to produce:
+
+1. Add your connection string for MSSQL Server DB in appsettings.json (Optional)
+
+   - Also update defaultConnectionString variable in Program.cs
--- src/ECourses.Web/Program.cs
+++ src/ECourses.Web/Program.cs
@@ -1,4 +1,13 @@
+using ECourses.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddSqlServer<ECoursesDbContext>(defaultConnectionString, 
+    o => o.MigrationsAssembly(typeof(ECoursesDbContext).Assembly.FullName));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
