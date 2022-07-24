1ff02fd8a6fedc3aa6a0aecb650ef40e1953ad30
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-24 18:13:54 +0300

    added Video entity

---
+++ src/ECourses.Data/Entities/Video.cs
@@ -0,0 +1,14 @@
+﻿using ECourses.Data.Common;
+
+namespace ECourses.Data.Entities
+{
+    public class Video : Entity
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+
+        public Guid CourseId { get; set; }
+        public Course Course { get; set; } = default!;
+
+    }
+}
