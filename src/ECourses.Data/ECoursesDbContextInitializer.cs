20073ca7ae8a523e02467283eb5f53c2a0defe60
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-24 23:29:25 +0300

    added DbContextInitializer to migrate and seed DB

---
+++ src/ECourses.Data/ECoursesDbContextInitializer.cs
@@ -0,0 +1,73 @@
+﻿using ECourses.Data.Exceptions;
+using ECourses.Data.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECourses.Data
+{
+    public class ECoursesDbContextInitializer
+    {
+        private readonly ECoursesDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public ECoursesDbContextInitializer(ECoursesDbContext context,
+            UserManager<User> userManager, 
+            RoleManager<Role> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task Initialize()
+        {
+            try
+            {
+                await _context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseInitializationException(ex);
+            }
+        }
+
+        public async Task Seed()
+        {
+            try
+            {
+                await SeedIdentity();
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseSeedingException(ex);
+            }
+        }
+
+        private async Task SeedIdentity()
+        {
+            var administratorRole = new Role
+            {
+                Name = "Administrator"
+            };
+
+            if (!await _roleManager.RoleExistsAsync(administratorRole.Name))
+            {
+                await _roleManager.CreateAsync(administratorRole);
+            }
+
+            var defaultUser = new User()
+            {
+                UserName = "Administrator",
+                Email = "admin@admin.com"
+            };
+
+            var administrators = await _userManager.GetUsersInRoleAsync(administratorRole.Name);
+
+            if (administrators.Count == 0)
+            {
+                await _userManager.CreateAsync(defaultUser, "admin!");
+            }
+        }
+    }
+}
--- src/ECourses.Web/Program.cs
+++ src/ECourses.Web/Program.cs
@@ -1,4 +1,5 @@
 using ECourses.Data;
+using ECourses.Data.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,23 @@ var defaultConnectionString = configuration.GetConnectionString("DefaultConnecti
 builder.Services.AddSqlServer<ECoursesDbContext>(defaultConnectionString, 
     o => o.MigrationsAssembly(typeof(ECoursesDbContext).Assembly.FullName));
 
+builder.Services.AddScoped<ECoursesDbContextInitializer>();
+
+builder.Services.AddIdentity<User, Role>(o =>
+{
+    o.Password.RequireDigit = false;
+    o.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<ECoursesDbContext>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContextInitializer = scope.ServiceProvider.GetRequiredService<ECoursesDbContextInitializer>();
+    await dbContextInitializer.Initialize();
+    await dbContextInitializer.Seed();
+}
 
 app.MapGet("/", () => "Hello World!");
 
