54931e73a61ecb791edf1b99d931f640c7db33c5
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-24 22:11:21 +0300

    added exceptions related to data access layer

---
+++ src/ECourses.Data/Exceptions/DatabaseInitializationException.cs
@@ -0,0 +1,11 @@
+﻿namespace ECourses.Data.Exceptions
+{
+    public class DatabaseInitializationException : Exception
+    {
+        private const string DefaultMessage = "Error occured during database initialization.";
+        public DatabaseInitializationException(string message) : base(message) { }
+        public DatabaseInitializationException(string message, Exception innerException) : base(message, innerException) { }
+        public DatabaseInitializationException(Exception innerException) : base(DefaultMessage, innerException) { }
+        public DatabaseInitializationException():base(DefaultMessage) { }
+    }
+}
---
+++ src/ECourses.Data/Exceptions/DatabaseSeedingException.cs
@@ -0,0 +1,11 @@
+﻿namespace ECourses.Data.Exceptions
+{
+    public class DatabaseSeedingException : Exception
+    {
+        private const string DefaultMessage = "Error occured during database seeding.";
+        public DatabaseSeedingException(string message):base(message) { }
+        public DatabaseSeedingException(string message, Exception innerException) : base(message, innerException) { }
+        public DatabaseSeedingException(Exception innerException):base(DefaultMessage, innerException) { }
+        public DatabaseSeedingException():base(DefaultMessage) { }
+    }
+}
