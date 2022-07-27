a463370e48f37fd34630af0be30fbd36702d4e9c
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-27 20:43:45 +0300

    added string.Format extension

---
+++ src/ECourses.ApplicationCore/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ECourses.ApplicationCore.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string F(this string stringToFormat, params object?[] args)
+        {
+            return string.Format(stringToFormat, args);
+        }
+    }
+}
