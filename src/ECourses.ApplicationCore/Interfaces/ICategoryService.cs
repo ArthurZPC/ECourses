0492c384447e767a78dc220c96e46408f7b3f57b
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-27 16:09:11 +0300

    added ICategoryService interface

---
+++ src/ECourses.ApplicationCore/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using ECourses.ApplicationCore.ViewModels;
+using ECourses.ApplicationCore.ViewModels.CreateViewModels;
+using ECourses.ApplicationCore.ViewModels.UpdateViewModels;
+
+namespace ECourses.ApplicationCore.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryViewModel>> GetAllCategories();
+        Task<CategoryViewModel?> GetCategoryById(Guid id);
+        Task Create(CreateCategoryViewModel model);
+        Task Delete(Guid id);
+        Task Update(Guid id, UpdateCategoryViewModel model);
+
+    }
+}
