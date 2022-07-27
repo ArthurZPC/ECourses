434a0659790724e54522c022917a4196f4e3a6b2
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-27 20:38:49 +0300

    added migration to apply category index

---
+++ src/ECourses.Data/Migrations/20220727162032_AddUniqueConstraintToCategories.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECourses.Data.Migrations
+{
+    public partial class AddUniqueConstraintToCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Title",
+                table: "Categories",
+                column: "Title",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Title",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
