e9a72603d69066b906f9884cd2e5fe57eda3f278
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-27 20:38:35 +0300

    added unique index to Category

---
+++ src/ECourses.Data/Entities/Configurations/CategoryEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECourses.Data.Entities.Configurations
+{
+    public class CategoryEntityTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasIndex(c => c.Title)
+                .IsUnique();
+        }
+    }
+}
