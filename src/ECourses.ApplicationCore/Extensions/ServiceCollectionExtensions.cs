6bbad03d7422846f912fdf712c10e082733ab362
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-28 17:51:17 +0300

    added required services registration

--- src/ECourses.ApplicationCore/Extensions/ServiceCollectionExtensions.cs
+++ src/ECourses.ApplicationCore/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,14 @@
-﻿using ECourses.ApplicationCore.Interfaces;
+﻿using ECourses.ApplicationCore.Converters;
+using ECourses.ApplicationCore.Interfaces;
+using ECourses.ApplicationCore.Interfaces.Converters;
+using ECourses.ApplicationCore.Interfaces.Services;
+using ECourses.ApplicationCore.Interfaces.Validators;
+using ECourses.ApplicationCore.Services;
+using ECourses.ApplicationCore.Validators;
 using ECourses.Data;
+using ECourses.Data.Common.Interfaces.Repositories;
 using ECourses.Data.Identity;
+using ECourses.Data.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ECourses.ApplicationCore.Extensions
@@ -32,6 +40,22 @@ namespace ECourses.ApplicationCore.Extensions
         public static IServiceCollection AddStartupTask<T>(this IServiceCollection services) where T :class, IStartupTask
         {
             services.AddTransient<IStartupTask, T>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<ICategoryConverter, CategoryConverter>();
+            services.AddTransient<ICategoryValidator, CategoryValidator>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryService, CategoryService>();
 
             return services;
         }
