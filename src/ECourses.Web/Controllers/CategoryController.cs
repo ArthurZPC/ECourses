845741c255a07220005a6f0e10d5ca9a1e19a9c7
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-29 22:59:55 +0300

    updated categoryController

---
+++ src/ECourses.Web/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using ECourses.ApplicationCore.Interfaces.Services;
+using ECourses.ApplicationCore.ViewModels.CreateViewModels;
+using ECourses.ApplicationCore.ViewModels.UpdateViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECourses.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllCategories();
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryViewModel model)
+        {
+            await _categoryService.Create(model);
+
+            return CreatedAtAction(nameof(Create), model);
+        }
+
+        [HttpDelete("{id:Guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _categoryService.Delete(id);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateCategoryViewModel model)
+        {
+            await _categoryService.Update(model);
+
+            return Ok(model);
+        }
+    }
+}
--- src/ECourses.Web/Program.cs
+++ src/ECourses.Web/Program.cs
@@ -1,5 +1,7 @@
 using ECourses.ApplicationCore.Extensions;
 using ECourses.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,15 +12,50 @@ builder.Services
     .ConfigureDatabase(defaultConnectionString)
     .ConfigureIdentity();
 
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
 builder.Services
     .AddStartupTasks()
+    .AddGenericEntityValidator()
     .AddRepositories()
     .AddApplicationServices()
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen(o =>
+    {
+        o.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "ECourses",
+            Version = "v1",
+            Description = "This DEMO app created for selling courses using ASP.NET Core",
+            Contact = new OpenApiContact
+            {
+                Email = "guseynoff.artur@yandex.ru",
+                Name = "Artur Huseinau",
+            }
+        });
+    })
     .AddControllers();
 
 var app = builder.Build();
 
 await app.RunStartupTasks();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(o =>
+{
+    o.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    o.RoutePrefix = String.Empty;
+});
+
+app.UseStaticFiles();
 
 app.MapControllers();
 
