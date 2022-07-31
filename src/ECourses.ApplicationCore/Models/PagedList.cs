e3c2ba6695ec02773f4773765fd38a15b399a8df
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-07-31 20:27:08 +0300

    added paged list model

---
+++ src/ECourses.ApplicationCore/Models/PagedList.cs
@@ -0,0 +1,9 @@
+﻿namespace ECourses.ApplicationCore.Models
+{
+    public class PagedList<T> 
+    {
+        public int Count { get; set; }
+
+        public IEnumerable<T> Items { get; set; } = new List<T>();
+    }
+}
