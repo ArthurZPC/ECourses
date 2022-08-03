3d4c2d87dab28707f726e3e5e1c2300a8c0dca31
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-08-03 20:35:07 +0300

    added Author configuration and migration

---
+++ src/ECourses.Data/Entities/Configurations/AuthorEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ECourses.Data.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECourses.Data.Entities.Configurations
+{
+    public class AuthorEntityTypeConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasIndex(a => new { a.FirstName, a.LastName })
+                .IsUnique();            
+        }
+    }
+}
---
+++ src/ECourses.Data/Migrations/20220803173421_AddIndexesToAuthors.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECourses.Data.Migrations
+{
+    public partial class AddIndexesToAuthors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Authors",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Authors",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_FirstName_LastName",
+                table: "Authors",
+                columns: new[] { "FirstName", "LastName" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Authors_FirstName_LastName",
+                table: "Authors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
