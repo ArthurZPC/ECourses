def66530ce5e25f9316263423358c34593a39e53
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-08-04 00:51:49 +0300

    configured rating entity and added migrations

---
+++ src/ECourses.Data/Entities/Configurations/RatingEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECourses.Data.Entities.Configurations
+{
+    public class RatingEntityTypeConfiguration : IEntityTypeConfiguration<Rating>
+    {
+        public void Configure(EntityTypeBuilder<Rating> builder)
+        {
+            builder.HasIndex(r => new { r.CourseId, r.UserId })
+                .IsUnique();
+        }
+    }
+}
--- src/ECourses.Data/Migrations/20220803214735_AddedUserToRatingsTable.Designer.cs
+++ src/ECourses.Data/Migrations/20220803214735_AddedUserToRatingsTable.Designer.cs
@@ -4,6 +4,7 @@ using ECourses.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECourses.Data.Migrations
 {
     [DbContext(typeof(ECoursesDbContext))]
-    partial class ECoursesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220803214735_AddedUserToRatingsTable")]
+    partial class AddedUserToRatingsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -139,10 +141,9 @@ namespace ECourses.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.HasIndex("UserId");
+                    b.HasIndex("CourseId");
 
-                    b.HasIndex("CourseId", "UserId")
-                        .IsUnique();
+                    b.HasIndex("UserId");
 
                     b.ToTable("Ratings");
                 });
