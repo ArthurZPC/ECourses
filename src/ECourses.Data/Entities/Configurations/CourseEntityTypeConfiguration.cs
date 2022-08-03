e943c626ec699f00f6c8e862f53365aa5d7ee01c
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-08-03 23:09:44 +0300

    added courses configuration and migration

---
+++ src/ECourses.Data/Entities/Configurations/CourseEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECourses.Data.Entities.Configurations
+{
+    public class CourseEntityTypeConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasIndex(c => c.Title)
+                .IsUnique();
+        }
+    }
+}
