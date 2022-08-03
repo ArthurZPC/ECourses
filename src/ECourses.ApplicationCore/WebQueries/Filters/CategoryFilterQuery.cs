6eaceb8c63a0a8c8c659ca34898805066c2489a1
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-08-03 12:17:53 +0300

    added web queries

---
+++ src/ECourses.ApplicationCore/WebQueries/Filters/CategoryFilterQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECourses.ApplicationCore.WebQueries.Filters
+{
+    public class CategoryFilterQuery
+    {
+        [Display(Name = "title")]
+        public string? Title { get; set; }
+    }
+}
---
+++ src/ECourses.ApplicationCore/WebQueries/PaginationQuery.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECourses.ApplicationCore.WebQueries
+{
+    public class PaginationQuery
+    {
+        [Display(Name = "pageNumber")]
+        public int PageNumber { get; set; } = 1;
+
+        [Display(Name = "pageSize")]
+        public int PageSize { get; set; } = 100;
+    }
+}
