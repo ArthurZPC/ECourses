03cc990947b9188f280a47f3068353c4a2880880
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-08-03 12:17:34 +0300

    added category ordering query constants

---
+++ src/ECourses.ApplicationCore/Common/Constants/OrderQueryConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ECourses.ApplicationCore.Common.Constants
+{
+    public class CategoryOrderQueryConstants
+    {
+        public const string TitleAsc = "title_asc";
+        public const string TitleDesc = "title_desc";
+    }
+}
