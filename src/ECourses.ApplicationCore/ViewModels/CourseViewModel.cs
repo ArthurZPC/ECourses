082c9c5ef764e7dba0024fb2641804d5f6054a4c
Artur Huseinau <guseynoff.artur@yandex.ru>
2022-11-13 03:36:52 +0300

    refactored course and rating view model

--- src/ECourses.ApplicationCore/ViewModels/CourseViewModel.cs
+++ src/ECourses.ApplicationCore/ViewModels/CourseViewModel.cs
@@ -5,8 +5,8 @@
         public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public DateTime PublishedAt { get; set; }
-        public decimal Price { get; set; }
+        public DateTime? PublishedAt { get; set; }
+        public decimal? Price { get; set; }
 
         public Guid AuthorId { get; set; }
         public Guid CategoryId { get; set; }
--- src/ECourses.ApplicationCore/ViewModels/RatingViewModel.cs
+++ src/ECourses.ApplicationCore/ViewModels/RatingViewModel.cs
@@ -3,7 +3,7 @@
     public class RatingViewModel
     {
         public Guid Id { get; set; }
-        public int Value { get; set; }
+        public int? Value { get; set; }
 
         public Guid CourseId { get; set; }
         public Guid UserId { get; set; }
